be68413a5a6683adc91a6a12b5b081662e98fd36
Om Tambat <omtambat284@gmail.com>
2025-09-09 21:34:35 +0530

    Web app added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -58,18 +58,3 @@ class DataIngestion:
 
         except Exception as e:
             CustomException(e, sys)
-
-
-# if __name__ == "__main__":
-
-#     data_ingestion_config = DataIngestionConfig()
-#     data_ingestion = DataIngestion(data_ingestion_config)
-
-#     train_data, test_data = data_ingestion.initiate_data_ingestion()
-
-#     data_transformation = DataTransformation()
-#     train_arr, test_arr = data_transformation.initiate_data_transformation(train_data, test_data)
-    
-#     model_trainer_config = ModelTrainerConfig()
-#     model_trainer = ModelTrainer()
-#     print(model_trainer.initiate_model_trainer(train_arr, test_arr))
